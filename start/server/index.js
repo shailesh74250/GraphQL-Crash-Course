1a4e9c85703206a0797a2e0692f6126df119fd45
shailesh74250 <smourya7425@gmail.com>
2021-08-13 17:52:57 +0530

    Initial commit

---
+++ start/server/index.js
@@ -0,0 +1,14 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
